7b4c30a1fdcda149e10198e583c3b9d11efc968b
bethemf <be_fulford@yahoo.co.nz>
2019-03-06 10:04:46 +1300

    change 1

--- MyFirst Project/src/HelloWorldExample.java
+++ MyFirst Project/src/HelloWorldExample.java
@@ -2,7 +2,7 @@
 public class HelloWorldExample {
 
 	public static void main(String[] args) {
-		//TODO Auto-generated method stub
-		System.out.println("Hllo World");
+		//Output Hello World to the user
+		System.out.println("Hello World");
 	}
 }
